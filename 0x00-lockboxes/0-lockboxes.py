1eb090a89dd56511c67a26257e46a61343912ae9
HermesBoots <Samuel.of.Hermes@GMail.com>
2020-02-04 16:43:26 -0500

    Add type checking to lockboxes project

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -2,6 +2,12 @@
 
 def canUnlockAll(boxes):
     """Determine whether keys for all boxes are accessible."""
+    if (
+        not isinstance(boxes, list) or
+        not all(isinstance(box, list) for box in boxes) or
+        not all(isinstance(index, int) for box in boxes for index in box)
+    ):
+        return False
     newKeys = set(boxes[0])
     keys = newKeys.union((0,))
     indexes = set(range(len(boxes)))
