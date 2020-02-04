dfd5be5c2b8346f85b68803875052d43d2446e2e
HermesBoots <Samuel.of.Hermes@GMail.com>
2020-02-04 16:39:36 -0500

    Handle out-of-bound keys in lockbox project

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 
 def canUnlockAll(boxes):
+    """Determine whether keys for all boxes are accessible."""
     newKeys = set(boxes[0])
     keys = newKeys.union((0,))
+    indexes = set(range(len(boxes)))
     while len(newKeys) > 0:
         index = newKeys.pop()
-        box = set(boxes[index]) - keys
+        if index < 0 or index >= len(boxes):
+            continue
+        box = set(boxes[index]) - keys & indexes
         newKeys |= box
         keys |= box
     return len(keys) == len(boxes)
