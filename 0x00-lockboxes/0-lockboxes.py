20848b09eaa91fda6c799ff3d9aff24559ea6d62
HermesBoots <Samuel.of.Hermes@GMail.com>
2020-02-04 18:44:11 -0500

    Apply extraneous key filter to first lockbox

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -10,9 +10,9 @@ def canUnlockAll(boxes):
         not all(isinstance(index, int) for box in boxes for index in box)
     ):
         return False
-    newKeys = set(boxes[0])
-    allKeys = newKeys.union((0,))
     indexes = set(range(len(boxes)))
+    newKeys = set(boxes[0]) & indexes
+    allKeys = newKeys.union((0,))
     while len(newKeys) > 0:
         index = newKeys.pop()
         if index < 0 or index >= len(boxes):
