d4ff7bc3329c50950a250cc90f122817e4b8d405
HermesBoots <Samuel.of.Hermes@GMail.com>
2020-03-03 12:48:21 -0500

    Add project to build and collapse piles of sand

---
+++ 0x04-sandpiles/0-sandpiles.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+
+void print_grid_3x3(int grid[3][3])
+{
+	int x, y;
+
+	printf("=\n");
+	for (y = 0; y < 3; y++)
+	{
+		for (x = 0; x < 3; x++)
+			printf(x < 2 ? "%d " : "%d", grid[y][x]);
+		printf("\n");
+	}
+}
+
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+	unsigned char stable = 1;
+	int x, y;
+
+	for (y = 0; y < 3; y++)
+		for (x = 0; x < 3; x++)
+		{
+			grid1[y][x] += grid2[y][x];
+			grid2[y][x] = 0;
+			if (grid1[y][x] > 3)
+				stable = 0;
+		}
+	if (stable)
+		return;
+
+	print_grid_3x3(grid1);
+	for (y = 0; y < 3; y++)
+		for (x = 0; x < 3; x++)
+		{
+			if (grid1[y][x] > 3)
+			{
+				grid1[y][x] -= 4;
+				if (y > 0)
+					grid2[y-1][x]++;
+				if (x > 0)
+					grid2[y][x-1]++;
+				if (x < 2)
+					grid2[y][x+1]++;
+				if (y < 2)
+					grid2[y+1][x]++;
+			}
+		}
+	sandpiles_sum(grid1, grid2);
+}
---
+++ 0x04-sandpiles/sandpiles.h
@@ -0,0 +1,7 @@
+#ifndef _SANDPILES_H_
+#define _SANDPILES_H_
+
+void print_grid_3x3(int grid[3][3]);
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+
+#endif
