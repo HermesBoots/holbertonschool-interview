9033046ad78ab9d8f53affb00c3ae54cc52c6071
HermesBoots <Samuel.of.Hermes@GMail.com>
2020-04-01 05:27:49 -0400

    Add integer palindrome project

---
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,21 @@
+/**
+ * is_palindrome - check if an integer is a palindrome
+ * @n: integer to check
+ * Return: 1 if n is palindrome, 0 otherwise
+ */
+int is_palindrome(unsigned long n)
+{
+	unsigned long left = 10000000000000000000LU;
+	unsigned long right = 1;
+
+	while (n / left == 0 && left > 0)
+		left /= 10;
+	while (left > right)
+	{
+		if (n / left % 10 != n / right % 10)
+			return (0);
+		left /= 10;
+		right *= 10;
+	}
+	return (1);
+}
---
+++ 0x08-palindrome_integer/README.md
@@ -0,0 +1,2 @@
+# Palindrome Integer
+Determine whether an integer, when printed in base 10, is a palindrome.
