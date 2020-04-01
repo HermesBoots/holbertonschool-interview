18c9b013c9fd0b9b71958a8b281456b2727a0b43
HermesBoots <Samuel.of.Hermes@GMail.com>
2020-04-01 06:10:09 -0400

    Manually flatten loop to find number of digits

--- 0x08-palindrome_integer/0-is_palindrome.c
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -1,4 +1,48 @@
-#include <math.h>
+/**
+ * count_digits - count the base-10 digits in an integer
+ * @n: integer to check
+ * Return: number of digits in n
+ */
+extern inline unsigned long count_digits(unsigned long n)
+{
+	if (n < 10LU)
+		return (1LU);
+	if (n < 100LU)
+		return (10LU);
+	if (n < 1000LU)
+		return (100LU);
+	if (n < 10000LU)
+		return (1000LU);
+	if (n < 100000LU)
+		return (10000LU);
+	if (n < 1000000LU)
+		return (100000LU);
+	if (n < 10000000LU)
+		return (1000000LU);
+	if (n < 100000000LU)
+		return (10000000LU);
+	if (n < 1000000000LU)
+		return (100000000LU);
+	if (n < 10000000000LU)
+		return (1000000000LU);
+	if (n < 100000000000LU)
+		return (10000000000LU);
+	if (n < 1000000000000LU)
+		return (100000000000LU);
+	if (n < 10000000000000LU)
+		return (1000000000000LU);
+	if (n < 100000000000000LU)
+		return (10000000000000LU);
+	if (n < 1000000000000000LU)
+		return (100000000000000LU);
+	if (n < 10000000000000000LU)
+		return (1000000000000000LU);
+	if (n < 100000000000000000LU)
+		return (10000000000000000LU);
+	if (n < 1000000000000000000LU)
+		return (100000000000000000LU);
+	return (1000000000000000000LU);
+}
 
 /**
  * is_palindrome - check if an integer is a palindrome
@@ -10,7 +54,7 @@ int is_palindrome(unsigned long n)
 	unsigned long left;
 	unsigned long right = 1;
 
-	left = pow(10, (unsigned long)log10(n));
+	left = count_digits(n);
 	while (left > right)
 	{
 		if (n / left % 10 != n / right % 10)
