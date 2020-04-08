b5ec54f6bcf1705747e58f278d8cf9c966799729
HermesBoots <Samuel.of.Hermes@GMail.com>
2020-04-08 04:50:54 -0400

    Add UTF-8 validation project

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""UTF-8 validation module"""
+
+
+def validUTF8(data):
+    """Validate whether a list of ints is valid UTF-8"""
+
+    if len(data) < 1:
+        return True
+    index = 0
+    while index < len(data):
+        if data[index] < 128:
+            index += 1
+        elif (
+            data[index] & 0xE0 == 0xC0 and
+            data[index + 1] & 0xC0 == 0x80
+        ):
+            index +=2 
+        elif (
+            data[index] & 0xF0 == 0xE0 and
+            data[index + 1] & 0xC0 == 0x80 and
+            data[index + 1] & 0xC0 == 0x80
+        ):
+            index += 3
+        elif (
+            data[index] & 0xF8 == 0xF0 and
+            data[index + 1] & 0xC0 == 0x80 and
+            data[index + 2] & 0xC0 == 0x80 and
+            data[index + 3] & 0xC0 == 0x80
+        ):
+            index += 4
+        else:
+            return False
+    return True
---
+++ 0x09-utf8_validation/README.md
@@ -0,0 +1,3 @@
+# UTF-8 Validation
+
+Determine whether a list of integers is a valid UTF-8 byte sequence.
