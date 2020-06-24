5e0729742b08441d9bda57c85cada6d22d5ffc0c
HermesBoots <Samuel.of.Hermes@GMail.com>
2020-06-24 15:46:23 -0400

    Add binary search project

---
+++ 0x12-advanced_binary_search/search_algos.h
@@ -0,0 +1,8 @@
+#ifndef _SEARCH_ALGOS_H_
+#define _SEARCH_ALGOS_H_
+
+void print_mem_range_advanced(int const *left, int const *right);
+int do_advanced_binary(int *array, size_t left, size_t right, int value);
+int advanced_binary(int *array, size_t size, int value);
+
+#endif
